ed9d7df9ae5a43d209b299c3e7a896d92828cead
Maddie Ford <maddiemford@gmail.com>
2020-06-21 15:42:32 -0400

    Updates

---
+++ precache-manifest.52cc9ce3418cb335189da2724df88627.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f47025086108a9841604bfe7e2d78b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "85a7e7b9ad6ec8b827fa",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "7b3345338268253d57c4",
+    "url": "/portfolio/static/css/main.38c8a1f0.chunk.css"
+  },
+  {
+    "revision": "85a7e7b9ad6ec8b827fa",
+    "url": "/portfolio/static/js/2.b0fa2a2c.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.b0fa2a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b3345338268253d57c4",
+    "url": "/portfolio/static/js/main.733aa150.chunk.js"
+  },
+  {
+    "revision": "518e4af0b06d9c1d56e9",
+    "url": "/portfolio/static/js/runtime-main.879528c7.js"
+  },
+  {
+    "revision": "cb274e41c097036aa484fc5c2fe9270c",
+    "url": "/portfolio/static/media/background.cb274e41.svg"
+  },
+  {
+    "revision": "eb94fe20d8f53c67c0bc508375738509",
+    "url": "/portfolio/static/media/doodle.eb94fe20.svg"
+  }
+]);
