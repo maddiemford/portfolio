01de407b25c7471852dbd18e062b7c9fab18d741
Maddie Ford <maddiemford@gmail.com>
2020-06-22 22:08:45 -0400

    Updates

--- precache-manifest.e6898bbdc748922d16b214c2cbad88e8.js
+++ precache-manifest.e6898bbdc748922d16b214c2cbad88e8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "46d43e9ee4cc3eafb29467d7c228d49b",
+    "revision": "1fce6700a54e98d71431592f64b7870f",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.e4346227.chunk.css"
   },
   {
-    "revision": "031cd0f36e4efeca937e",
-    "url": "/portfolio/static/css/main.6d2932ec.chunk.css"
+    "revision": "2a471bfd80038504b729",
+    "url": "/portfolio/static/css/main.e9e17331.chunk.css"
   },
   {
     "revision": "236f6e7585fea163d66c",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.0319c1d5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "031cd0f36e4efeca937e",
+    "revision": "2a471bfd80038504b729",
     "url": "/portfolio/static/js/main.9e9dd6a4.chunk.js"
   },
   {
@@ -28,8 +28,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/runtime-main.879528c7.js"
   },
   {
-    "revision": "b55cba2a13b579f6900d3733966303ca",
-    "url": "/portfolio/static/media/background.b55cba2a.png"
+    "revision": "8214df53a1bbf34f515b8117accaa3ca",
+    "url": "/portfolio/static/media/background.8214df53.png"
   },
   {
     "revision": "eb94fe20d8f53c67c0bc508375738509",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b83681b65ad481a26eae3e01e42d9e3b.js"
+  "/portfolio/precache-manifest.e6898bbdc748922d16b214c2cbad88e8.js"
 );
 
 self.addEventListener('message', (event) => {
